ca44024352ef054e324b7a7c65d5336b63d68bd7
v1p3rh0rr0r <v1p3rh0rr0r@protonmail.com>
2021-10-03 23:57:31 +0200

    reworked crypto mechanism to use hybrid encryption/decryption

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,9 @@
+use aes_gcm::aead::{generic_array::GenericArray, Aead, NewAead};
 use aes_gcm::Aes256Gcm;
 
 use contacts::*;
 use error::{CliErrorKind, ContactsErrorKind, CryptographicErrorKind, QryptexError};
 use rand::prelude::StdRng;
-use rand::rngs::OsRng;
 use rand::{RngCore, SeedableRng};
 use rsa::{pem::parse, pem::Pem, PaddingScheme, PublicKey, RSAPrivateKey, RSAPublicKey};
 use std::fs;
@@ -14,6 +14,10 @@ use std::{convert::TryFrom, path::Path};
 mod contacts;
 mod error;
 
+/// The encrypted data has the following format
+/// Nonce|Key|Ciphertext
+/// 12bytes|16bytes|...
+/// These first 28 bytes are RSA encrypted.
 fn main() {
     let mut op_settings = match read_cli_args() {
         Ok(s) => s,
@@ -159,9 +163,12 @@ fn encrypt(settings: &OpSettings, app: &AppSettings) -> Result<(), QryptexError>
             // build session key
             let (nonce, key) = generate_operation_primitives();
             // build aes cipher
-            let cipher = Aes256Gcm::new(key);
-            let ciphertext = encrypt_txt(&plaintext[..], &cipher, &nonce)?;
-            let encrypted_key = encrypt_symmetric_key(&key, &pub_key)?;
+            let mut cipher = Aes256Gcm::new(GenericArray::from_slice(&key));
+            let ciphertext = encrypt_plaintext(&plaintext[..], &mut cipher, &nonce)?;
+            let mut prefix = [0u8; 28];
+            prefix[..12].copy_from_slice(&nonce);
+            prefix[12..].copy_from_slice(&key);
+            let encrypted_prefix = encrypt_primitives(&prefix, &pub_key)?;
             if *is_path {
                 // output must be a file
                 let ciphertext_path = match output_path {
@@ -179,8 +186,9 @@ fn encrypt(settings: &OpSettings, app: &AppSettings) -> Result<(), QryptexError>
                     .map_err(|_| QryptexError::new_crypto(CryptographicErrorKind::Io))?;
             } else {
                 // output is a small string which should be written to stdout
-                let ciphertext_str = ciphertext
+                let ciphertext_str = encrypted_prefix
                     .iter()
+                    .chain(ciphertext.iter())
                     .map(|b| format!("{:02X}", b))
                     .collect::<Vec<String>>()
                     .join("");
@@ -201,15 +209,20 @@ fn decrypt(settings: &OpSettings, app: &AppSettings) -> Result<(), QryptexError>
             contact: _,
             output_path,
         } if settings.op == Operation::Decrypt => {
-            let ciphertext = match is_path {
+            let prefixed_ciphertext = match is_path {
                 true => std::fs::read(target)
                     .map_err(|_| QryptexError::new_crypto(CryptographicErrorKind::Io)),
                 false => hex_to_bytes(target),
             }?;
 
+            let (encrypted_prefix, ciphertext) =
+                (&prefixed_ciphertext[..28], &prefixed_ciphertext[28..]);
             let path = app.local_keys_path.join("private.pem");
             let private_key = load_private_key(path.as_path())?;
-            let plaintext_raw = decrypt_ciphertext(&ciphertext, &private_key)?;
+            let (nonce, key) = recover_primitives(encrypted_prefix, &private_key)?;
+
+            let mut cipher = Aes256Gcm::new(GenericArray::from_slice(&key));
+            let plaintext_raw = decrypt_ciphertext(&ciphertext, &mut cipher, &nonce)?;
             dbg!(&plaintext_raw);
             if *is_path {
                 // output must be a file
@@ -247,6 +260,24 @@ fn decrypt(settings: &OpSettings, app: &AppSettings) -> Result<(), QryptexError>
     }
 }
 
+fn recover_primitives(
+    prefix: &[u8],
+    private_key: &RSAPrivateKey,
+) -> Result<([u8; 12], [u8; 16]), QryptexError> {
+    if prefix.len() != 28 {
+        return Err(QryptexError::new_crypto(CryptographicErrorKind::Format));
+    }
+
+    let mut nonce = [0u8; 12];
+    let mut key = [0u8; 16];
+
+    let decrypted_prefix = decrypt_primitives(prefix, private_key)?;
+    nonce.copy_from_slice(&decrypted_prefix[..12]);
+    key.copy_from_slice(&decrypted_prefix[12..]);
+
+    Ok((nonce, key))
+}
+
 /// Generates a aes key to use for an encryption operation.
 fn generate_operation_primitives() -> ([u8; 12], [u8; 16]) {
     let mut nonce = [0u8; 12];
@@ -258,18 +289,18 @@ fn generate_operation_primitives() -> ([u8; 12], [u8; 16]) {
     (nonce, key)
 }
 
-///
-fn encrypt_symmetric_key(key: &[u8], public_key: &RSAPublicKey) -> Result<Vec<u8>, QryptexError> {
+/// Encrypts the aes key with the contacts public key.
+fn encrypt_primitives(key: &[u8], public_key: &RSAPublicKey) -> Result<Vec<u8>, QryptexError> {
     let mut rng = StdRng::from_entropy();
     let padding = PaddingScheme::new_oaep::<sha2::Sha256>();
-    match public_key.encrypt(&mut rng, padding, plaintext) {
+    match public_key.encrypt(&mut rng, padding, key) {
         Err(_) => Err(QryptexError::new_crypto(CryptographicErrorKind::Encryption)),
         Ok(ciph) => Ok(ciph),
     }
 }
 
 ///
-fn decrypt_symmetric_key(key: &[u8], private_key: &RSAPrivateKey) -> Result<Vec<u8>, QryptexError> {
+fn decrypt_primitives(key: &[u8], private_key: &RSAPrivateKey) -> Result<Vec<u8>, QryptexError> {
     let padding = PaddingScheme::new_oaep::<sha2::Sha256>();
     let decrypted_key = match private_key.decrypt(padding, key) {
         Err(_) => Err(QryptexError::new_crypto(CryptographicErrorKind::Decryption)),
@@ -279,21 +310,29 @@ fn decrypt_symmetric_key(key: &[u8], private_key: &RSAPrivateKey) -> Result<Vec<
     Ok(decrypted_key)
 }
 
-fn encrypt_txt(
+/// TODO: Make this streaming.
+fn encrypt_plaintext(
     plaintext: &[u8],
     cipher: &mut Aes256Gcm,
     nonce: &[u8; 12],
 ) -> Result<Vec<u8>, QryptexError> {
     // prepare encryption
-    cipher.encrypt(nonce, plaintext).expect("encryption failed");
-    Ok(vec![])
+    let ciphertext = cipher
+        .encrypt(GenericArray::from_slice(nonce), plaintext)
+        .expect("encryption failed");
+    Ok(ciphertext)
 }
 
+/// TODO: Make this streaming.
 fn decrypt_ciphertext(
     ciphertext: &[u8],
-    private_key: &RSAPrivateKey,
+    cipher: &mut Aes256Gcm,
+    nonce: &[u8; 12],
 ) -> Result<Vec<u8>, QryptexError> {
-    Ok(vec![])
+    let plaintext = cipher
+        .decrypt(GenericArray::from_slice(nonce), ciphertext)
+        .expect("decryption failed");
+    Ok(plaintext)
 }
 
 fn hex_to_bytes(hex: &str) -> Result<Vec<u8>, QryptexError> {
